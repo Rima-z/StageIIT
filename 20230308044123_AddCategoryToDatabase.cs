6369adfd0da12a887bef9b27f1969d4964cd57ef
Rima-z <124570482+Rima-z@users.noreply.github.com>
2024-07-19 08:10:58 +0100

    Add files via upload

---
+++ 20230308044123_AddCategoryToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFonctionToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fonctions",
+                columns: table => new
+                {
+                    CodeF = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Libelle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CodeF);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fonctions");
+        }
+    }
+}
